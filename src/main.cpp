63d0ced19361011f81f32ddef571ffc5c72e43d0
박시우 <atomicw63.546@gmail.com>
2025-01-22 16:58:19 +0900

    조종기 신호 읽기

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,111 @@
-// asdf
-
 #include <Arduino.h>
 #include <Wire.h>
-#include <Arduino_BMI270_BMM150.h>
+#include <Servo.h>
 
+#define CH1 5
+#define CH2 4
+#define CH3 3
+#define CH4 2
 
-// 칼만 필터를 위한 변수 선언
-float Q_angle = 0.001;	// 각도에 대한 프로세스 노이즈 공분산
-float Q_bias = 0.003;	// 바이어스에 대한 프로세스 노이즈 공분산
-float R_measure = 0.03; // 측정 노이즈 공분산
+volatile int CH1_pulse_width = 0;
+volatile int CH2_pulse_width = 0;
+volatile int CH3_pulse_width = 0;
+volatile int CH4_pulse_width = 0;
 
-float angle = 0.0;				  // 각도 추정값
-float bias = 0.0;				  // 바이어스 추정값
-float P[2][2] = {{0, 0}, {0, 0}}; // 공분산 행렬
+void pulsecount1()
+{
+	static unsigned long last_time = 0;
+	unsigned long current_time = micros();
+	if (digitalRead(CH1) == HIGH)
+	{
+		last_time = current_time;
+	}
+	else
+	{
+		CH1_pulse_width = current_time - last_time;
+	}
+}
+void pulsecount2()
+{
+	static unsigned long last_time = 0;
+	unsigned long current_time = micros();
+	if (digitalRead(CH2) == HIGH)
+	{
+		last_time = current_time;
+	}
+	else
+	{
+		CH2_pulse_width = current_time - last_time;
+	}
+}
+void pulsecount3()
+{
+	static unsigned long last_time = 0;
+	unsigned long current_time = micros();
+	if (digitalRead(CH3) == HIGH)
+	{
+		last_time = current_time;
+	}
+	else
+	{
+		CH3_pulse_width = current_time - last_time;
+	}
+}
+void pulsecount4()
+{
+	static unsigned long last_time = 0;
+	unsigned long current_time = micros();
+	if (digitalRead(CH4) == HIGH)
+	{
+		last_time = current_time;
+	}
+	else
+	{
+		CH4_pulse_width = current_time - last_time;
+	}
+}
 
-unsigned long lastTime;
-float dt;
+#define ESC_FRONT_LEFT 6
+#define ESC_FRONT_RIGHT 7
+#define ESC_BACK_RIGHT 8
+#define ESC_BACK_LEFT 9
+
+#define ESC_MIN 1000
+#define ESC_MAX 2000
+
+Servo esc[4];
+// Pins connected to ESCs
+const int escPins[] = {ESC_FRONT_LEFT, ESC_FRONT_RIGHT, ESC_BACK_RIGHT, ESC_BACK_LEFT};
+const int numEscs = 4;
 
 void setup()
 {
 	Serial.begin(115200);
-	while (!Serial)
-		;
 
-	// 센서 초기화
-	if (!IMU.begin())
+	pinMode(CH1, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(CH1), pulsecount1, CHANGE);
+	pinMode(CH2, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(CH2), pulsecount2, CHANGE);
+	pinMode(CH3, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(CH3), pulsecount3, CHANGE);
+	pinMode(CH4, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(CH4), pulsecount4, CHANGE);
+
+	for (int i = 0; i < numEscs; i++)
 	{
-		Serial.println("Failed to initialize IMU!");
-		while (1)
-			;
+		esc[i].attach(escPins[i], ESC_MIN, ESC_MAX);
 	}
-	Serial.println("IMU initialized successfully.");
-
-	lastTime = millis();
 }
 
 void loop()
 {
-	float accelX, accelY, accelZ;
-	float gyroX, gyroY, gyroZ;
-
-	// 가속도 데이터 읽기
-	if (IMU.accelerationAvailable())
-	{
-		IMU.readAcceleration(accelX, accelY, accelZ);
-	}
-
-	// 자이로스코프 데이터 읽기
-	if (IMU.gyroscopeAvailable())
-	{
-		IMU.readGyroscope(gyroX, gyroY, gyroZ);
-	}
-
-	// 시간 간격 계산
-	unsigned long currentTime = millis();
-	dt = (currentTime - lastTime) / 1000.0;
-	lastTime = currentTime;
-
-	// 가속도계를 이용한 각도 계산
-	float accelAngle = atan2(accelY, accelZ) * RAD_TO_DEG;
-
-	// 칼만 필터 적용
-	float rate = gyroX - bias;
-	angle += dt * rate;
-
-	// 공분산 행렬 업데이트
-	P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
-	P[0][1] -= dt * P[1][1];
-	P[1][0] -= dt * P[1][1];
-	P[1][1] += Q_bias * dt;
-
-	// 측정 업데이트
-	float S = P[0][0] + R_measure;
-	float K[2];
-	K[0] = P[0][0] / S;
-	K[1] = P[1][0] / S;
-
-	float y = accelAngle - angle;
-	angle += K[0] * y;
-	bias += K[1] * y;
-
-	// 공분산 행렬 업데이트
-	float P00_temp = P[0][0];
-	float P01_temp = P[0][1];
-
-	P[0][0] -= K[0] * P00_temp;
-	P[0][1] -= K[0] * P01_temp;
-	P[1][0] -= K[1] * P00_temp;
-	P[1][1] -= K[1] * P01_temp;
-
-	// 결과 출력
-	Serial.print("Angle: ");
-	Serial.println(angle);
-
-	delay(10); // 100Hz 주기 유지
+	Serial.print("CH1: ");
+	Serial.print(CH1_pulse_width);
+	Serial.print("\tCH2: ");
+	Serial.print(CH2_pulse_width);
+	Serial.print("\tCH3: ");
+	Serial.print(CH3_pulse_width);
+	Serial.print("\tCH4: ");
+	Serial.print(CH4_pulse_width);
+	Serial.println();
 }
